49055042c6d3cc6b131e84594620c9cf63c608de
vishnutt <vishnutt@xminds.com>
2024-03-07 17:01:46 +0530

    getter and setter of student details with external class

--- App/TypeScriptScreen.ts
+++ App/TypeScriptScreen.ts
@@ -1,14 +1,49 @@
-//npx ts-node TypeScriptScreen.ts
+//npx ts-node TypeScriptScreen.ts -- for run
+
+import { StudentInfo } from "../App/StudentInfo";
 
 const hello = () => {
   return sum(6, 2);
 };
 
-let sum = (x: number, y: number): number => {
+let sum1 = (x: number, y: number): number => {
   let result = x + y;
   return result;
 };
 
+let sum = (x: number, y: number): number => x + y;
+
+// (rollno,course,duration,start date,end date,garde)
+type Student = {
+  name: string;
+  rollno: number;
+  course: string;
+  duration: number;
+  startDate: string;
+  endDate: string;
+  grade: string;
+};
+
+const studentDetails: Student = {
+  name: "Vishnu",
+  rollno: 30,
+  course: "Nodejs Training",
+  duration: 3,
+  startDate: "06-03-2024",
+  endDate: "06-04-2024",
+  grade: "",
+};
+
+let stdInfo = new StudentInfo(studentDetails);
+
+//setGrade
+stdInfo.setGrade = "G+";
+
+// setDuration and setRollNumber
+stdInfo.setDurations = 10;
+stdInfo.setRollNumber = 33;
+
 console.log("====================================");
-console.log(hello());
+console.log(stdInfo);
+console.log(stdInfo.getFullDetails());
 console.log("====================================");
